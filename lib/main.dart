3dbda32b1ff258f02444dfb98b254d7b28f77255
Hitesh <joshihitesh090@gmail.com>
2022-02-17 00:10:09 +0530

    integrated with user provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
-import 'package:firebase_auth/screens/login/login_screen.dart';
+import 'package:firebase_auth_test/providers/user_provider.dart';
+import 'package:firebase_auth_test/screens/home/home_screen.dart';
+import 'package:firebase_auth_test/screens/login/login_screen.dart';
+import 'package:firebase_auth_test/screens/signup/signup_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +24,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: LoginScreen(),
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+      },
+      initialRoute: '/',
     );
   }
 }
